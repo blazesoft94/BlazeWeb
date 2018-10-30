bf0c3aa028468036cc7fc120f523e5807f6ae0b7
blazesoft94 <abdul.rokr@gmail.com>
2018-10-30 14:57:12 +0500

    check

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname+"/public"));
 
+app.get("/check", function(req,res){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ _network_available: true }));
+});
+
 server.listen(port,()=>{
     console.log("listening on",port);
 });
